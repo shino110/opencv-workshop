e87352eafc2d42fded0f7f4e7ef2db9dd2d0078e
Shino Ito <ito.s.cd@m.titech.ac.jp>
2024-04-09 12:53:25 +0900

    20240409 lec: SURF not done

--- py/lucas_kanade_optical_flow.py
+++ py/lucas_kanade_optical_flow.py
@@ -41,8 +41,8 @@ while(1):
         for i,(new,old) in enumerate(zip(good_new,good_old)):
             a,b = new.ravel()
             c,d = old.ravel()
-            mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-            frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
+            mask = cv2.line(mask, (int(a),int(b)),(int(c),int(d)), color[i].tolist(), 2)
+            frame = cv2.circle(frame,(int(a),int(b)),5,color[i].tolist(),-1)
         img = cv2.add(frame,mask)
 
         cv2.imshow('frame',img)
